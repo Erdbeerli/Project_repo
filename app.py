2f6b5d3a826dd2274bc39b4df936d81c660a19dc
Erdbeerli <medeamuriel.schillig@student.unisg.ch>
2024-10-28 19:35:44 +0100

    almost

--- app.py
+++ app.py
@@ -2,9 +2,4 @@ import streamlit as st
 
 st.write("yessssss it works!")
 st.write("this is so cool")
-<<<<<<< HEAD
 st.write("I think it works")
-
-=======
-
->>>>>>> 67d6581de05c61b3ad2263f9f71fd30b9f8c2ca4
