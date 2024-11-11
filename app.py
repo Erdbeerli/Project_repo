9bb9f0bb0e56bf2275026990d2e4ff2c636ccfd7
Erdbeerli <medeamuriel.schillig@student.unisg.ch>
2024-11-11 13:24:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def search_bar():
    """Diese Funktion erlaubt eine gezielte Suchanfrage für eine Pflanze."""
    
    #import requests is needed here
-   st.title("Find vegetable informations here")
+   st.title("🔎Find vegetable informations here")
    st.write("Search for a specific vegetable or herb and find out more about the plant details.")
 
    #Input field for vegetable/herb name
